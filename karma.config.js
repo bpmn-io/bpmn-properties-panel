425652cfa0339b72c88ca67879b148220cffe989
Maciej Barelkowski <maciej.barelkowski@camunda.com>
2021-10-06 19:55:03 +0200

    wip

--- karma.config.js
+++ karma.config.js
@@ -14,12 +14,14 @@ const singleStart = process.env.SINGLE_START;
 
 const coverage = process.env.COVERAGE;
 
+const esbuild = !!process.env.ESBUILD;
+
 const absoluteBasePath = path.resolve(path.join(__dirname, basePath));
 
 // use puppeteer provided Chrome for testing
 process.env.CHROME_BIN = require('puppeteer').executablePath();
 
-const suite = coverage ? 'test/coverageBundle.js' : 'test/testBundle.js';
+const suite = coverage ? 'test/coverageBundle.js' : 'test/spec/**/*.spec.js';
 
 module.exports = function(karma) {
 
@@ -28,7 +30,6 @@ module.exports = function(karma) {
     basePath,
 
     frameworks: [
-      'webpack',
       'mocha',
       'sinon-chai'
     ],
@@ -38,12 +39,12 @@ module.exports = function(karma) {
     ],
 
     preprocessors: {
-      [ suite ]: [ 'webpack', 'env' ]
+      [ suite ]: [ 'env' ]
     },
 
     reporters: [ 'progress' ].concat(coverage ? 'coverage' : []),
 
-    coverageReporter: {
+    coverageReporter: coverage && {
       reporters: [
         { type: 'lcov', subdir: '.' },
       ]
@@ -53,95 +54,13 @@ module.exports = function(karma) {
 
     singleRun: true,
     autoWatch: false,
-
-    webpack: {
-      mode: 'development',
-      module: {
-        rules: [
-          {
-            test: /\.(css|bpmn)$/,
-            use: 'raw-loader'
-          },
-          {
-            test: /\.m?js$/,
-            exclude: /node_modules/,
-            use: {
-              loader: 'babel-loader',
-              options: {
-                plugins: [
-                  [ '@babel/plugin-transform-react-jsx', {
-                    'importSource': 'preact',
-                    'runtime': 'automatic'
-                  } ]
-                ]
-              }
-            }
-          },
-          {
-            test: /\.svg$/,
-            use: [ 'react-svg-loader' ]
-          }
-        ].concat(coverage ?
-          {
-            test: /\.js$/,
-            use: {
-              loader: 'istanbul-instrumenter-loader',
-              options: { esModules: true }
-            },
-            enforce: 'post',
-            include: /src\.*/,
-            exclude: /node_modules/
-          } : []
-        )
-      },
-      plugins: [
-        new DefinePlugin({
-
-          // @barmac: process.env has to be defined to make @testing-library/preact work
-          'process.env': {}
-        }),
-        new NormalModuleReplacementPlugin(
-          /^preact(\/[^/]+)?$/,
-          function(resource) {
-
-            const replMap = {
-              'preact/hooks': path.resolve('node_modules/preact/hooks/dist/hooks.module.js'),
-              'preact/jsx-runtime': path.resolve('node_modules/preact/jsx-runtime/dist/jsxRuntime.module.js'),
-              'preact': path.resolve('node_modules/preact/dist/preact.module.js')
-            };
-
-            const replacement = replMap[resource.request];
-
-            if (!replacement) {
-              return;
-            }
-
-            resource.request = replacement;
-          }
-        ),
-        new NormalModuleReplacementPlugin(
-          /^preact\/hooks/,
-          path.resolve('node_modules/preact/hooks/dist/hooks.module.js')
-        )
-      ],
-      resolve: {
-        mainFields: [
-          'browser',
-          'module',
-          'main'
-        ],
-        alias: {
-          'react': 'preact/compat',
-          'react-dom': 'preact/compat'
-        },
-        modules: [
-          'node_modules',
-          absoluteBasePath
-        ]
-      },
-      devtool: 'eval-source-map'
-    }
   };
+
+  if (esbuild) {
+    setupEsbuild(config);
+  } else {
+    setupWebpack(config);
+  }
 
   if (singleStart) {
     config.browsers = [].concat(config.browsers, 'Debug');
@@ -150,3 +69,130 @@ module.exports = function(karma) {
 
   karma.set(config);
 };
+
+function setupWebpack(config) {
+  const webpackConfig = {
+    mode: 'development',
+    module: {
+      rules: [
+        {
+          test: /\.(css|bpmn)$/,
+          use: 'raw-loader'
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              plugins: [
+                [ '@babel/plugin-transform-react-jsx', {
+                  'importSource': 'preact',
+                  'runtime': 'automatic'
+                } ]
+              ]
+            }
+          }
+        },
+        {
+          test: /\.svg$/,
+          use: [ 'react-svg-loader' ]
+        }
+      ].concat(coverage ?
+        {
+          test: /\.js$/,
+          use: {
+            loader: 'istanbul-instrumenter-loader',
+            options: { esModules: true }
+          },
+          enforce: 'post',
+          include: /src\.*/,
+          exclude: /node_modules/
+        } : []
+      )
+    },
+    plugins: [
+      new DefinePlugin({
+
+        // @barmac: process.env has to be defined to make @testing-library/preact work
+        'process.env': {}
+      }),
+      new NormalModuleReplacementPlugin(
+        /^preact(\/[^/]+)?$/,
+        function(resource) {
+
+          const replMap = {
+            'preact/hooks': path.resolve('node_modules/preact/hooks/dist/hooks.module.js'),
+            'preact/jsx-runtime': path.resolve('node_modules/preact/jsx-runtime/dist/jsxRuntime.module.js'),
+            'preact': path.resolve('node_modules/preact/dist/preact.module.js')
+          };
+
+          const replacement = replMap[resource.request];
+
+          if (!replacement) {
+            return;
+          }
+
+          resource.request = replacement;
+        }
+      ),
+      new NormalModuleReplacementPlugin(
+        /^preact\/hooks/,
+        path.resolve('node_modules/preact/hooks/dist/hooks.module.js')
+      )
+    ],
+    resolve: {
+      mainFields: [
+        'browser',
+        'module',
+        'main'
+      ],
+      alias: {
+        'react': 'preact/compat',
+        'react-dom': 'preact/compat'
+      },
+      modules: [
+        'node_modules',
+        absoluteBasePath
+      ]
+    },
+    devtool: 'eval-source-map'
+  };
+
+  config.frameworks.push('webpack');
+  config.webpack = webpackConfig;
+  config.preprocessors[ suite ].push('webpack');
+
+}
+
+function setupEsbuild(config) {
+
+  const esbuildConfig = {
+    define: {
+
+      // @barmac: process.env has to be defined to make @testing-library/preact work
+      'process.env': '{}'
+    },
+
+    jsxFactory: 'React.h',
+    jsxFragment: 'React.Fragment',
+
+    plugins: [
+      require('esbuild-plugin-svgr')(),
+      require('esbuild-plugin-alias')({
+        'react': 'preact/compat',
+        'react-dom': 'preact/compat'
+      })
+    ],
+
+    loader: {
+      '.css': 'text',
+      '.bpmn': 'text'
+    },
+
+    singleBundle: true
+  };
+
+  config.preprocessors[ suite ].push('esbuild');
+  config.esbuild = esbuildConfig;
+}
